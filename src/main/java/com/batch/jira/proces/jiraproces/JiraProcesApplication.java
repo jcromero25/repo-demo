9ba017c99a63e2a479b6c447561ce89c213911f7
Juan Carlos Romero <jcromero04@icloud.com>
2018-10-23 15:12:47 -0500

    firt cmommnit

---
+++ src/main/java/com/batch/jira/proces/jiraproces/JiraProcesApplication.java
@@ -0,0 +1,12 @@
+package com.batch.jira.proces.jiraproces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiraProcesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiraProcesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/batch/jira/proces/jiraproces/JiraProcesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.batch.jira.proces.jiraproces;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JiraProcesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
